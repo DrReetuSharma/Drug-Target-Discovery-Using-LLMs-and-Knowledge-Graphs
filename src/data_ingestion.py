53a7789b7636faf72688b9d93c0f34d71232736c
DrReetuSharma <162538254+DrReetuSharma@users.noreply.github.com>
2024-05-27 23:28:52 +0530

    Update data_ingestion.py

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def clean_data(df):
+    # Implement cleaning steps
+    return df
+
+if __name__ == "__main__":
+    raw_data = load_data('data/raw/dataset_HTN.csv')
+    cleaned_data = clean_data(raw_data)
+    cleaned_data.to_csv('data/processed/processed_HTN.csv', index=False)
